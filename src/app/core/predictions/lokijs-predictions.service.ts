f7b3a21ccf4cae8a66180b4d46a224d7cec00d3c
juniorschen <juniorschen@hotmail.com>
2024-08-29 11:02:26 -0300

    Fix: possivel null

--- src/app/core/predictions/lokijs-predictions.service.ts
+++ src/app/core/predictions/lokijs-predictions.service.ts
@@ -34,7 +34,6 @@ export class LokiJsPredictionsService {
     }
 
     async doAddWordOnDb(word: string) {
-        word.trim().length > 0
         const collection = this.dbLoki.getCollection(word.charAt(0));
 
         const topRegistersOnCollection = collection.chain()
